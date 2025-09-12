60eff1773dabe1a7a7be9d37a31ed34a99f42038
Drew Sumsion <drewsumsion10@gmail.com>
2025-09-12 09:34:27 -0600

    add keypoint normalization

--- third_party_helpers/access_fsrt.py
+++ third_party_helpers/access_fsrt.py
@@ -122,10 +122,10 @@ class access_fsrt:
             # driving_frame = driving_video.permute(0, 3, 1, 2).cuda()
             kp_driving, expression_vector_driv = extract_keypoints_and_expression(driving_frame.clone(), model, kp_detector,cfg)
             
-            # kp_norm = normalize_kp(kp_source=kp_source[0], kp_driving=kp_driving,
-            #                     kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
-            #                     adapt_movement_scale=adapt_movement_scale)
-            kp_norm = kp_driving
+            kp_norm = normalize_kp(kp_source=kp_source[0], kp_driving=kp_driving,
+                                kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
+                                adapt_movement_scale=adapt_movement_scale)
+            # kp_norm = kp_driving
 
             expression_vector_src = expression_vector_src.squeeze(1)
             expression_vector_src = expression_vector_src.repeat(driving_video.shape[0], 1, 1)
@@ -142,21 +142,26 @@ class access_fsrt:
     def run_fsrt_list_batch(self, source_images, driving_videos, save=False):
         '''
         Inputs:
-            source_images: np.array of np.array images of shape (B, 256, 256, 3)
-            driving_videos: list of list of np.array images of shape (B, T, 256, 256, 3)
+            source_images: np.array of np.array images of shape (B, 256, 256, 3)--the identity images
+            driving_videos: list of list of np.array images of shape (B, T, 256, 256, 3)--the expression videos
             save: boolean of whether to save the video as result
         '''
         driving_videos = driving_videos.float()
         predictions = self.make_animation_batch(np.array([source_images]), driving_videos, self.model, self.kp_detector, relative=self.relative, adapt_movement_scale=self.adapt_scale, cfg=self.cfg, max_num_pixels=self.max_num_pixels)
 
-        if save:
-            for i, frame in enumerate(predictions):
-                print("frame.shape:", frame.shape)
-                imageio.imwrite(f"output/frame_{i}.png", img_as_ubyte(frame.permute(1, 2, 0).cpu().numpy()))
-
-        predictions = [self.resize_transform(x) for x in predictions]
+        predictions = [torch.clamp(self.resize_transform(x), 0, 1) for x in predictions]
         predictions = torch.stack(predictions)
 
+        if save:
+            for i, frame in enumerate(predictions):
+                print("frame.shape:", frame.shape, torch.max(frame), torch.min(frame), frame.dtype)
+                print("source_images.shape:", source_images.shape, np.max(source_images), np.min(source_images), source_images.dtype)
+                print("driving_videos.shape:", driving_videos.shape, torch.max(driving_videos), torch.min(driving_videos), driving_videos.dtype)
+
+                imageio.imwrite(f"output/{i}_frame.png", img_as_ubyte(frame.permute(1, 2, 0).cpu().numpy()))
+                imageio.imwrite(f"output/{i}_source.png", img_as_ubyte(source_images))
+                imageio.imwrite(f"output/{i}_driving.png", img_as_ubyte(driving_videos[i].cpu().numpy()))
+            raise Exception("Saved frames")
         return predictions
 
     def run_fsrt_image_video(self, source_image_path, driving_video_path):
